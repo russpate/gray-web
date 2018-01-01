4fef865571908cac45e37239a0d7e17d7b5f472d
Russell Pate <russ@russpate.com>
2018-01-01 17:27:53 -0500

    article is live adding live links and connected everything

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
       var commitmentConvert = encodeURI(commitmentText);
       console.log(commitmentConvert);
 
-      $(".twitter-share-button").attr("href", "https://twitter.com/intent/tweet?text=" + "Let's do it. This year I'm going gray for " + commitmentConvert +". Read the article by @russpate " + "https://www.google.com/")
+      $(".twitter-share-button").attr("href", "https://twitter.com/intent/tweet?text=" + "Let's do it. This year I'm going gray for " + commitmentConvert +". Read the article by @russpate " + "https://medium.com/@russpate/the-practice-of-being-gray-ee21808b6d87")
   });
 
 });
