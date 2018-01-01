33faae89752b14668a27ca70c7c6dddc88bb153e
Russell Pate <russ@russpate.com>
2018-01-01 15:47:43 -0500

    added style, rearranged info, rewrote small copy, and replaced input for textarea

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ $( document ).ready(function() {
       var commitmentText = commitmentInput;
       var commitmentConvert = encodeURI(commitmentText);
       console.log(commitmentConvert);
-      $(".twitter-share-button").attr("href", "https://twitter.com/intent/tweet?text=" + "I commit to going gray for " + commitmentConvert + ". Read the article by @russpate " + "https://www.google.com/")
+
+      $(".twitter-share-button").attr("href", "https://twitter.com/intent/tweet?text=" + "Let's do it. This year I'm going gray for " + commitmentConvert +". Read the article by @russpate " + "https://www.google.com/")
   });
+
 });
